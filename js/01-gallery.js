098e0150a71806b11a474e0d35e99b86f2237099
Vitalii Protsko <provitaleks@gmail.com>
2023-01-19 17:59:27 +0200

    Делаем дз

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// import * as basicLightbox from 'basiclightbox'
+
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `)
+
+// instance.show()
+
 console.log(galleryItems);
+
+const refs = {
+    gallery: document.querySelector('.gallery'),
+};
+
+const listItemsMarkup = createListItemsMarkup(galleryItems);
+
+refs.gallery.innerHTML = listItemsMarkup;
+
+function createListItemsMarkup(items) {
+    return items.map(item => `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+            <img 
+                class="gallery__image"
+                src="${item.preview}"
+                data-source="${item.original}"
+                alt="Image description"
+            />
+        </a>
+    </div>
+    `).join('');
+}
+
